49d6c2fff5d9751c6fbcc8ee76ae0e6ca9230466
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-06-19 14:17:36 +0100

    testing out fixes for deployment error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 
 import './sass/main.scss';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
 import Shortener from './components/Shortener';
 import Features from './components/Features';
 import BoostSection from './components/BoostSection';
