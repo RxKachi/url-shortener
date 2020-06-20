2cbcfad9241e3be76ffc141412e5e22b490395e4
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-06-20 15:37:06 +0100

    optimisation of footer component for desktop view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,65 +6,70 @@ const Footer = (props) => {
   return (
     <footer className="footer">
       <Logo />
-      <div className="link-listing">
-        <h3 className="heading">Features</h3>
-        <ul className="links">
-          <li className="link">
-            <a href="#!">Link Shortening</a>
-          </li>
-          <li className="link">
-            <a href="#!">Branded Links</a>
-          </li>
-          <li className="link">
-            <a href="#!">Analytics</a>
-          </li>
-        </ul>
+      <div className='footer-links'>
+        <div className="link-listing">
+          <h3 className="heading">Features</h3>
+          <ul className="links">
+            <li className="link">
+              <a href="#!">Link Shortening</a>
+            </li>
+            <li className="link">
+              <a href="#!">Branded Links</a>
+            </li>
+            <li className="link">
+              <a href="#!">Analytics</a>
+            </li>
+          </ul>
+        </div>
+        <div className="link-listing">
+          <h3 className="heading">Resources</h3>
+          <ul className="links">
+            <li className="link">
+              <a href="#!">Blog</a>
+            </li>
+            <li className="link">
+              <a href="#!">Developers</a>
+            </li>
+            <li className="link">
+              <a href="#!">Support</a>
+            </li>
+          </ul>
+        </div>
+        <div className="link-listing">
+          <h3 className="heading">Company</h3>
+          <ul className="links">
+            <li className="link">
+              <a href="#!">About</a>
+            </li>
+            <li className="link">
+              <a href="#!">Our Team</a>
+            </li>
+            <li className="link">
+              <a href="#!">Careers</a>
+            </li>
+            <li className="link">
+              <a href="#!">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="socials">
+          <img src={FbImg} alt="" />
+          <img src={TwitImg} alt="" />
+          <img src={PinImg} alt="" />
+          <img src={InstaImg} alt="" />
+        </div>
       </div>
-      <div className="link-listing">
-        <h3 className="heading">Resources</h3>
-        <ul className="links">
-          <li className="link">
-            <a href="#!">Blog</a>
-          </li>
-          <li className="link">
-            <a href="#!">Developers</a>
-          </li>
-          <li className="link">
-            <a href="#!">Support</a>
-          </li>
-        </ul>
-      </div>
-      <div className="link-listing">
-        <h3 className="heading">Company</h3>
-        <ul className="links">
-          <li className="link">
-            <a href="#!">About</a>
-          </li>
-          <li className="link">
-            <a href="#!">Our Team</a>
-          </li>
-          <li className="link">
-            <a href="#!">Careers</a>
-          </li>
-          <li className="link">
-            <a href="#!">Contact</a>
-          </li>
-        </ul>
-      </div>
-      <div className="socials">
-          <img src={FbImg} alt=""/>
-          <img src={TwitImg} alt=""/>
-          <img src={PinImg} alt=""/>
-          <img src={InstaImg} alt=""/>
-
-      </div>
-      <div className="attribution">
+      {/* <div className="attribution">
         Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Frontend Mentor
         </a>
         . Coded by <a href="https://paulonyekwelu.com">Paul Onyekwelu</a>.
-      </div>
+      </div> */}
     </footer>
   );
 };
